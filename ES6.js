beec94bbf199ff1f56d14963017dae0c1e8b1757
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-08-07 00:21:40 +0800

    Add files via upload

---
+++ ES6.js
@@ -0,0 +1,53 @@
+// 2020, 8/05, pm & 8/06 in MRT train way to office
+
+import React, {Component} from 'react';
+
+class ES_Six extends React.Component{
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            a:0,
+            b:{x: 0, y:0},
+            c:"",
+            d:[],
+            e:{}
+        };
+        //如下免去 bind()
+        //因為使用 ES6 語法糖
+    }
+
+    componentDidUpdate(){
+
+        this.setState({
+            a: 1,
+            b:{x:11, y:12},
+            c: 'haha',
+            d: ["bread","tea","coffee","steamed food"],
+            e:{}
+        })
+
+    }
+
+    // 語法 ES 寫法
+    // 自動 bind()
+    funcCalled = () => {
+
+        this.setState({a:12})
+
+    }
+
+    render(){
+
+        const {g, gg} = this.state; // function component 才能用useState()
+
+        return(
+            <div>
+                <p>{this.state.b.x}</p>
+                <p> {this.funcCalled()}</p>
+                <button onClick={this.funcCalled}></button>
+            </div>
+        )
+    }
+}
